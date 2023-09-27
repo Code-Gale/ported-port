18292fee36c039cac86896f7bd6bd11271181ebc
Code-Gale <codewhiz001@gmail.com>
2023-09-27 23:26:04 +0100

    updating projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Media Downloader"
+              description="Online gallery that contains image media for a particular event and can then be downloaded to local storage via the site. Built with only HTMl and CSS"
+              ghLink="https://github.com/Code-Gale/web"
+              demoLink="https://ttogmedia.netlify.app/"              
             />
           </Col>
 
