6d794c45d5c26102f81538a81650e79f44ca4809
Code-Gale <codewhiz001@gmail.com>
2023-09-28 10:21:34 +0100

    latest update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,16 +92,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/Code-Gale/ported-port"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
